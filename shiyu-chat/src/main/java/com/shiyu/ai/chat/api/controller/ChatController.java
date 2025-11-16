665e4b196f4d1c0008aa441cc3127d3f5c1a9985
shiyu <876703634@qq.com>
2025-11-16 12:12:48 +0800

    fix

--- shiyu-chat/src/main/java/com/shiyu/ai/chat/api/controller/ChatController.java
+++ shiyu-chat/src/main/java/com/shiyu/ai/chat/api/controller/ChatController.java
@@ -1,11 +1,11 @@
 package com.shiyu.ai.chat.api.controller;
+
 import com.shiyu.ai.chat.core.context.GlobalContext;
 import com.yomahub.liteflow.core.FlowExecutor;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -15,12 +15,12 @@ public class ChatController {
     @Resource
     private FlowExecutor flowExecutor;
 
-    @PostMapping
-    public Map<String, Object> chat(@RequestBody Map<String, Object> request) {
+    @GetMapping
+    public Map<String, Object> chat(Map<String, Object> request) {
         GlobalContext context = new GlobalContext();
         try {
             context.set(GlobalContext.ChatBizKeyEnum.QUERY.getCode(), request.get("text"));
-            flowExecutor.execute2Resp("chatFlow", context);
+            flowExecutor.execute2Resp("chatFlow", null, context);
             String noSolution = context.get(GlobalContext.ChatBizKeyEnum.FINAL_ANSWER.getCode(), "no_solution");
             return Collections.singletonMap("result", noSolution);
 
