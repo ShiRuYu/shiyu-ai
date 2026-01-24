8d12aa730104b67dabd0a4e905cdadc539ac944d
li21 <876703634@qq.com>
2026-01-24 18:56:22 +0800

    code commit

--- shiyu-common/shiyu-common-core/src/main/java/com/shiyu/ai/common/core/config/ShiYuConfig.java
+++ shiyu-common/shiyu-common-core/src/main/java/com/shiyu/ai/common/core/config/ShiYuConfig.java
@@ -1,9 +1,11 @@
 package com.shiyu.ai.common.core.config;
 
 import com.shiyu.ai.common.core.domain.ShiYuDefaultExceptionHandler;
+import com.shiyu.ai.common.core.tx.TransactionTemplateExecutor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @AutoConfiguration
 @EnableConfigurationProperties(ShiYuProperties.class)
@@ -11,5 +13,9 @@ public class ShiYuConfig {
     @Bean
     public ShiYuDefaultExceptionHandler defaultExceptionHandler() {
         return new ShiYuDefaultExceptionHandler();
+    }
+    @Bean
+    public TransactionTemplateExecutor transactionTemplateExecutor(PlatformTransactionManager transactionManager) {
+        return new TransactionTemplateExecutor(transactionManager);
     }
 }
