c16e04126209daceac32a42fa525addd120d751b
li21 <876703634@qq.com>
2026-01-25 10:45:31 +0800

    code commit

--- shiyu-common/shiyu-common-core/src/main/java/com/shiyu/ai/common/core/config/ShiYuConfig.java
+++ shiyu-common/shiyu-common-core/src/main/java/com/shiyu/ai/common/core/config/ShiYuConfig.java
@@ -3,6 +3,7 @@ package com.shiyu.ai.common.core.config;
 import com.shiyu.ai.common.core.domain.ShiYuDefaultExceptionHandler;
 import com.shiyu.ai.common.core.tx.TransactionTemplateExecutor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -15,6 +16,7 @@ public class ShiYuConfig {
         return new ShiYuDefaultExceptionHandler();
     }
     @Bean
+    @ConditionalOnBooleanProperty(prefix = "shiyu.tx", name = "enabled", havingValue = true)
     public TransactionTemplateExecutor transactionTemplateExecutor(PlatformTransactionManager transactionManager) {
         return new TransactionTemplateExecutor(transactionManager);
     }
